c3443e99d3067cde635caaa1cc31239e0272f8c0
phyalexander <phy.alex@mail.ru>
2024-12-15 21:44:14 +0300

    Working on __getitem__ and __setitem__

--- manager.py
+++ manager.py
@@ -82,20 +82,6 @@ class Matrix3d(abc.ABC):
         self._shape = shape
         self.__cache = Cache(file_path, cache_shape)
  
-    def __getitem__(self, *index: int) -> float:
-        if len(index) == 1:
-            if (0 > index[0]) or (index > self._shape[0]):
-                return self._cache[index[0]]
-            else:
-                raise IndexError(f'Index {index[0]} is out of bounds [0..{self._shape[0]}]')
-        elif len(index) == 2:
-
-
-        return self._get(*index)
-
-    def __setitem__(self, index: Index, item: float) -> None:
-        self._set(*index, item)
-
     @abc.abstractmethod
     def _get(self, i: int, j: int, k: int) -> float:
         pass
@@ -103,6 +89,23 @@ class Matrix3d(abc.ABC):
     @abc.abstractmethod
     def _set(self, i: int, j: int, k: int, item: float) -> None:
         pass
+
+    def __getitem__(self, *index: int) -> float:
+        self.__check_index(index[0], self._shape[0])
+        if len(index) > 1:
+            self.__check_index(index[1], self._shape[1])
+            if len(index) > 2:
+                self.__check_index(index[2], self._shape[2])
+                return self._get(*index)
+            else:
+                raise IndexError(f"Wrong number of indexes: {len(index)} > 3")
+
+
+
+
+
+    def __setitem__(self, index: Index, item: float) -> None:
+        self._set(*index, item)
     
     def __mul__(self, value: "float | Matrix2d | Matrix3d") -> None:
         self.__operator(value, lambda x, y: x * y, 'multiply')
@@ -115,6 +118,63 @@ class Matrix3d(abc.ABC):
 
     def __div__(self, value: "float | Matrix2d | Matrix3d") -> None:
         self.__operator(value, lambda x, y: x / y, 'divide')
+
+
+    def get_2d_matrix(self, i: int) -> np.ndarray:
+        Matrix3d.__check_index(i, self._shape[0])
+        return self.__cache[i]
+
+    def fmap(self, function: Callable[[float], float]) -> None:
+        (l, m, n) = self._shape
+        for i in range(l):
+            for j in range(m):
+                for k in range(n):
+                    # self[(i, j, k)] = function(self[(i, j, k)])
+                    self._set(i,j,k, function(self._get(i,j,k)))
+
+    @property
+    def shape(self) -> tuple[int, int, int]:
+        return self._shape
+
+    def copy(self, name: str) -> "Matrix3d":
+        # klass = type(self)
+        # new = klass(name, self._shape)
+        # n = self._shape[0]
+        # for i in range(n):
+        #     new.__cache[i] = self.__cache[i]
+        # return new
+        new = type(self)(name, self._shape)
+        shutil.copytree(self.directory, new.directory, dirs_exist_ok=True)
+        new.__cache.reload() # otherwise cache will rewrite [0]-matrix with zeros
+        return new
+
+    @property
+    def directory(self) -> str:
+        return self.__cache.path_start[:-1]
+
+    def remove_from_disk(self) -> None:
+        self.__cache.remove_from_disk()
+
+    def reslice_by(self, dim: int, name: str) -> "Matrix3d":
+        if dim == 1:
+            nova_class = SlicedBy1
+        elif dim == 2:
+            nova_class = SlicedBy2
+        elif dim == 3:
+            nova_class = SlicedBy3
+        else:
+            raise ValueError(f"Wrong dimension: {dim}. Can be only 1, 2 or 3")
+
+        if type(self) == nova_class:
+            return self.copy(name)
+        new = nova_class(name, self._shape)
+        l, m, n = self._shape
+        for i in range(l):
+            for j in range(m):
+                for k in range(n):
+                    # new[(i, j, k)] = self[(i, j, k)]
+                    new._set(i,j,k, self._get(i,j,k))
+        return new
 
     def __operator(self, value: "float | Matrix2d | Matrix3d",
                    operator: Callable[[float, float], float],
@@ -164,7 +224,6 @@ class Matrix3d(abc.ABC):
         else:
             raise ValueError(f"Can't {op_name} Matrix3d by {type(value)}")
 
-
     def __validate(self, path: str, shape: tuple[int, int, int]) -> None:
         js_file = path + 'info.json'
         if os.path.exists(path):
@@ -178,64 +237,10 @@ class Matrix3d(abc.ABC):
             with open(js_file, 'w') as file:
                 json.dump({'shape' : list(shape)}, file)    
 
-    def get_2d_matrix(self, i: int) -> np.ndarray:
-        if (i > 0) and (i < self._shape[0]):
-            return self.__cache[i]
-        else:
-            raise IndexError(f'Index {i} is out of bounds [0..{self._shape[0]}]')
-
-    def fmap(self, function: Callable[[float], float]) -> None:
-        (l, m, n) = self._shape
-        for i in range(l):
-            for j in range(m):
-                for k in range(n):
-                    # self[(i, j, k)] = function(self[(i, j, k)])
-                    self._set(i,j,k, function(self._get(i,j,k)))
-
-
-    @property
-    def shape(self) -> tuple[int, int, int]:
-        return self._shape
-
-    def copy(self, name: str) -> "Matrix3d":
-        # klass = type(self)
-        # new = klass(name, self._shape)
-        # n = self._shape[0]
-        # for i in range(n):
-        #     new.__cache[i] = self.__cache[i]
-        # return new
-        new = type(self)(name, self._shape)
-        shutil.copytree(self.directory, new.directory, dirs_exist_ok=True)
-        new.__cache.reload() # otherwise cache will rewrite [0]-matrix with zeros
-        return new
-
-    @property
-    def directory(self) -> str:
-        return self.__cache.path_start[:-1]
-
-    def remove_from_disk(self) -> None:
-        self.__cache.remove_from_disk()
-
-    def reslice_by(self, dim: int, name: str) -> "Matrix3d":
-        if dim == 1:
-            nova_class = SlicedBy1
-        elif dim == 2:
-            nova_class = SlicedBy2
-        elif dim == 3:
-            nova_class = SlicedBy3
-        else:
-            raise ValueError(f"Wrong dimension: {dim}. Can be only 1, 2 or 3")
-
-        if type(self) == nova_class:
-            return self.copy(name)
-        new = nova_class(name, self._shape)
-        l, m, n = self._shape
-        for i in range(l):
-            for j in range(m):
-                for k in range(n):
-                    # new[(i, j, k)] = self[(i, j, k)]
-                    new._set(i,j,k, self._get(i,j,k))
-        return new
+    @staticmethod
+    def __check_index(i: int, up: int) -> None:
+        if (i < 0) or (i >= up):
+            raise IndexError(f'Index {i} is out of bounds [0..{up}]')
 
 
 
